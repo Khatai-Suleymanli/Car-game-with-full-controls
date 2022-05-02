c1651addf9b6e356a58bd3ff42c2a5cc21cc14de
Khatai-Suleymanli <80199997+Khatai-Suleymanli@users.noreply.github.com>
2022-05-02 23:08:59 +0400

    Update PlayerController.cs
    This is the code file for giving the player object movement

--- PlayerController.cs
+++ PlayerController.cs
@@ -7,8 +7,8 @@ public class PlayerController : MonoBehaviour
 {
    //Private variables
 
-    private float speed = 25.0f;  // *20 yazmaq yerine speed deyiseni ile ede bilerik
-    private float turnspeed = 55.0f;  //masinin donmesini temin eden deyisen
+    private float speed = 25.0f;
+    private float turnspeed = 55.0f; 
     private float horizontalInput;
     private float forwardInput;
 
@@ -27,18 +27,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //PLAYER INPUTU ALIRIQ BURADA
+       
 
-        // Input.GetAxis edib horizontalInput deyisenine controllar veririk
-        // "Horizontal" unity ayarlarinda input managerde donme controlunun adidir.
+        // Input.GetAxis - to get input key
+        // "Horizontal" means horizontal arrows keys
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-
-
-        //We will move the wehicle forward
-        //Translate cismi hereket etdirir; Vector3.forward ise translate(0, 0, 1) qisalmasidir, saniyede 60 m gedir.
-        // sureti ayarlamaq ucun Time.deltaTime istifade edirik, bele olanda developerin ayarladigi suret nedirese onunla gedir
-        // sureti artirmaq ucun ise * x kimi ede bilerik ve ya sped variable yaradib ede bilerik.
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
 
@@ -48,7 +42,6 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(Vector3.up, Time.deltaTime * turnspeed * horizontalInput);
 
 
-        // HARDDDDD
         if (Input.GetKeyDown(switchKey))
         {
             mainCamera.enabled = !mainCamera.enabled;
